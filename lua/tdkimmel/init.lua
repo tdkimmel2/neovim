ceb5c4624790014c225f508750e3e6a33e8969cf
tdkimmel2 <tdkimmel2@gmail.com>
2025-02-11 20:21:32 -0500

    Added useful go to definition remap that can open the definition in a split. Also added go to error commands.

--- lua/tdkimmel/init.lua
+++ lua/tdkimmel/init.lua
@@ -1,3 +1,27 @@
 require("tdkimmel.set")
 require("tdkimmel.remap")
 require("tdkimmel.lazy_init")
+
+local augroup = vim.api.nvim_create_augroup
+local tdkimmelGroup = augroup('tdkimmel', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd('LspAttach', {
+    group = tdkimmelGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "gvd", function()
+            vim.cmd([[vsplit]])
+            vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "ghd", function()
+            vim.cmd([[split]])
+            vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+        --Diagnostics
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
+})
