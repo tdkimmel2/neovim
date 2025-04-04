ab6ea62eaa1ca9e3b2a087ff73056d87e88e10bc
tdkimmel2 <tdkimmel2@gmail.com>
2025-04-04 15:49:39 -0400

    Configured tree-sitter to work with vimtex by disabling latex highlighting.

--- lua/tdkimmel/lazy/vimtex.lua
+++ lua/tdkimmel/lazy/vimtex.lua
@@ -5,6 +5,6 @@ return {
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = "zathura"
-    vim.g.vimtex_compiler_method = "pdflatex"
+    vim.g.vimtex_compiler_method = "latexmk"
   end
 }
